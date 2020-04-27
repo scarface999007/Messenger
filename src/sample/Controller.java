de971c01f2d0e3cf909b394e0fd2d6d43f72d3cb
bvmarinyak <bvmarinyak@gmail.com>
2020-04-27 15:19:59 +0300

    HomeWork4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+import java.util.Date;
+
 public class Controller {
+
+    public TextArea textArea;
+    public TextField textField;
+
+    public void sendMessage(){
+        if(!textField.getText().isEmpty()){
+            if(textArea.getText().isEmpty()){
+                textArea.setText(String.valueOf(new Date()));
+            } else {
+                textArea.appendText(String.valueOf(new Date()));
+            }
+            textArea.appendText(": " + textField.getText() + "\n");
+            textField.setText("");
+        }
+    }
+
+    public void send(ActionEvent actionEvent) {
+        sendMessage();
+    }
+
+    public void keyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode().getName().equals("Enter")){
+            sendMessage();
+        }
+    }
 }
